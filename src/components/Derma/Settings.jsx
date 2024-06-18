5d7c64be53bac3193f3daa8e0a443edd6dac7daa
huzaifa003 <buntyhuzaifa3@gmail.com>
2024-06-18 19:22:56 +0500

    Added Change Password To It

--- src/components/Derma/Settings.jsx
+++ src/components/Derma/Settings.jsx
@@ -1,23 +1,33 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { View, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
-import { Avatar, List, Button, Divider } from "react-native-paper";
+import { Avatar, List, Button, Divider, TextInput } from "react-native-paper";
 import { auth, storage } from "../../Connection/DB";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import * as ImagePicker from 'expo-image-picker';
-import { updateProfile } from "firebase/auth";
-
+import { updatePassword, updateProfile } from "firebase/auth";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 const Settings = () => {
   const navigation = useNavigation()
   const [userData, setUserData] = useState({});
+  const [password, setPassword] = useState('');
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisibility(!passwordVisibility);
+  };
   useEffect(() => {
+
+
+
+
     const getUserData = async () => {
       const email = await AsyncStorage.getItem('email');
       const uid = await AsyncStorage.getItem('uid');
       const userType = await AsyncStorage.getItem('userType');
 
-      
+
 
       if (AsyncStorage.getItem('photoURL') !== null) {
         const photoURL = await AsyncStorage.getItem('photoURL');
@@ -59,66 +69,79 @@ const Settings = () => {
         console.error(error);
       });
 
-      
+
     // Implement your logout logic here
   };
 
-  
-const selectImage = async () => {
-  const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-  if (status !== 'granted') {
-    alert('Sorry, we need camera roll permissions to make this work!');
-    return;
-  }
 
-  let result = await ImagePicker.launchImageLibraryAsync({
+  const selectImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+      return;
+    }
 
-    mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    allowsEditing: true,
-    aspect: [4, 3],
-    quality: 1,
-  });
+    let result = await ImagePicker.launchImageLibraryAsync({
 
-  console.log(result);
-
-  if (!result.canceled) {
-    const response = await fetch(result.assets[0].uri);
-    const blob = await response.blob();
-    const uid = await AsyncStorage.getItem('uid');
-    const pictureRef = ref(storage,`images/${uid}/profile.jpg`);
-
-    console.log(response);
-
-    console.log(blob);
-
-    const res = await uploadBytes(pictureRef, blob);
-
-    console.log(res);
-
-    const downloadableLink = await getDownloadURL(pictureRef);
-    console.log(downloadableLink);
-
-    AsyncStorage.setItem('photoURL', downloadableLink);
-    setUserData({
-      ...userData,
-      avatarUrl: downloadableLink,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
 
-    updateProfile(auth.currentUser, {
-      photoURL: downloadableLink,
-    })
-    .then(() => {
-      console.log('Profile updated successfully');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    console.log(result);
+
+    if (!result.canceled) {
+      const response = await fetch(result.assets[0].uri);
+      const blob = await response.blob();
+      const uid = await AsyncStorage.getItem('uid');
+      const pictureRef = ref(storage, `images/${uid}/profile.jpg`);
+
+      console.log(response);
+
+      console.log(blob);
+
+      const res = await uploadBytes(pictureRef, blob);
+
+      console.log(res);
+
+      const downloadableLink = await getDownloadURL(pictureRef);
+      console.log(downloadableLink);
+
+      AsyncStorage.setItem('photoURL', downloadableLink);
+      setUserData({
+        ...userData,
+        avatarUrl: downloadableLink,
+      });
+
+      updateProfile(auth.currentUser, {
+        photoURL: downloadableLink,
+      })
+        .then(() => {
+          console.log('Profile updated successfully');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
 
 
-    
+
+    }
   }
-}
 
+  const changePassword = () => {
+    // Implement your change password logic here
+    updatePassword(auth.currentUser, password)
+      .then(() => {
+        alert('Password updated successfully');
+        console.log('Password updated successfully');
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('An error occurred while updating your password');
+      });
+
+  }
 
 
   return (
@@ -136,25 +159,50 @@ const selectImage = async () => {
       <Divider />
       <List.Section>
         <List.Subheader>User Settings</List.Subheader>
-        {/* <List.Item
+        <List.Item
           title="Username"
           description={userData.name}
           left={(props) => <List.Icon {...props} icon="account-circle" />}
-        /> */}
-        {/* <List.Item
-          title="Change Password"
-          left={(props) => <List.Icon {...props} icon="lock-reset" />}
-          onPress={() => console.log("Change Password Pressed")}
-        /> */}
+        />
+
+
         <List.Item
           title="Email"
           description={userData.email}
           left={(props) => <List.Icon {...props} icon="email" />}
         />
+
+        <List.Item
+          title="Change Password"
+          left={(props) => <List.Icon {...props} icon="lock-reset" />}
+
+        />
+
+        <TextInput
+          style={{ marginHorizontal: 20 }}
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={passwordVisibility}
+          mode="outlined"
+          right={<TextInput.Icon name="menu-down" onPress={togglePasswordVisibility} />} // Ensures the icon is clickable to open the menu
+        />
+
+        <Button
+
+
+          onPress={changePassword}
+          icon="lock-reset"
+          // textColor="#0584fa" // Sets the background color of the button
+          mode='contained'
+          style={styles.logoutButton}
+        >
+          Change Password
+        </Button>
       </List.Section>
       <Divider />
       <List.Section>
-        <List.Subheader>Account</List.Subheader>
+        
         <Button
 
 
