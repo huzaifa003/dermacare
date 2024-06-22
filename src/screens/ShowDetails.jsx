b442660d4254f1dc73b11e82ff27097a90bf4933
huzaifa003 <buntyhuzaifa3@gmail.com>
2024-06-23 03:17:32 +0500

    Added Markdown to Show Details

--- src/screens/ShowDetails.jsx
+++ src/screens/ShowDetails.jsx
@@ -4,10 +4,42 @@ import { Card, Title, Paragraph, Avatar, Button, useTheme } from 'react-native-p
 import GeneralHeader from '../components/GeneralHeader';
 import { Ionicons } from '@expo/vector-icons';
 import * as Sharing from 'expo-sharing';
+import Markdown from 'react-native-markdown-display';
 const ShowDetails = ({ route }) => {
   const { id, status, recommendation, feedback, pdf, image } = route.params;
   const { colors } = useTheme(); // Use theme for color adaptability
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+    },
+    card: {
+      margin: 10,
+      elevation: 4,
+      backgroundColor: colors.secondary,
+    },
+    cover: {
+      height: 200,
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: colors.background,
+    },
+    subtitle: {
+      fontSize: 18,
+      marginTop: 10,
+      marginBottom: 5,
+      fontWeight: 'bold',
+    },
+    paragraph: {
+      fontSize: 16,
+      lineHeight: 24,
+      marginBottom: 10,
+      color: colors.secondary,
+    },
+  });
   
   const handleDownloadPdf = () => {
     Linking.openURL(pdf); // Opens the PDF URL in the user's default web browser
@@ -36,7 +68,7 @@ const ShowDetails = ({ route }) => {
         <Card.Cover source={{ uri: image }} style={styles.cover} />
         <Card.Content>
           <Title style={[styles.subtitle, {color: colors.primary}]}>Recommendation</Title>
-          <Paragraph style={styles.paragraph}>{recommendation}</Paragraph>
+          <Markdown  style={[styles.paragraph, {body: { color: colors.primary}}]}>{recommendation}</Markdown>
           <Title style={[styles.subtitle, {color: colors.primary}]}>Feedback</Title>
           <Paragraph style={styles.paragraph}>{feedback}</Paragraph>
         </Card.Content>
@@ -55,33 +87,6 @@ const ShowDetails = ({ route }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-  card: {
-    margin: 10,
-    elevation: 4,
-  },
-  cover: {
-    height: 200,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 18,
-    marginTop: 10,
-    marginBottom: 5,
-    fontWeight: 'bold',
-  },
-  paragraph: {
-    fontSize: 16,
-    lineHeight: 24,
-    marginBottom: 10,
-  },
-});
+
 
 export default ShowDetails;
